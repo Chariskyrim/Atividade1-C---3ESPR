5766f6401b426c1fef82f07ade9134717ffe8801
Chariskyrim <enzolucianox@gmail.com>
2026-08-24 22:29:38 -0300

    feat: adiciona leitura dos dados do cliente no console

--- Atividade C# 1 - 3ESPR/Program.cs
+++ Atividade C# 1 - 3ESPR/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace CineCidade
 {
@@ -6,6 +7,21 @@ namespace CineCidade
     {
         static void Main()
         {
+            var cultura = new CultureInfo("pt-BR");
+
+            Console.WriteLine("=== CineCidade - Venda de Ingressos ===\n");
+
+            Console.Write("Nome do cliente: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Quantidade de ingressos: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Console.Write("Cupom de desconto: ");
+            string cupom = Console.ReadLine();
+
+            Console.Write("Idade do cliente: ");
+            int idade = int.Parse(Console.ReadLine());
         }
     }
 }
